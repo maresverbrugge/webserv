82e1cf254c18a97aab0da70e4d5bf199036b7f16
Flen Huisman <fhuisman@student.codam.nl>
2024-04-11 16:35:38 +0200

    stay alive is dead now

--- tests/getaddrinfo_test.cpp
+++ tests/getaddrinfo_test.cpp
@@ -13,20 +13,14 @@
 
 #define MAX_EVENTS 10
 
-void sigHandler(int sig)
-{
-    if (sig == SIGINT)
-    {
-        std::cout << "SIGINT" << std::endl;
-        close(3); // assuming server_socket = 3 (Need to fix that later)
-        exit(0);
-    }
-}
+int server_socket;
+int epoll_fd;
 
 void getservinfo(struct addrinfo *servinfo)
 {
     struct addrinfo hints;
     struct addrinfo *res;
+    struct addrinfo *p;
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;     // AF_UNSPEC for IPv4 and IPv6
@@ -39,38 +33,62 @@ void getservinfo(struct addrinfo *servinfo)
         perror(gai_strerror(status));
         exit(EXIT_FAILURE);
     }
-    servinfo->ai_flags = res->ai_flags;
-    servinfo->ai_family = res->ai_family;
-    servinfo->ai_socktype = res->ai_socktype;
-    servinfo->ai_protocol = res->ai_protocol;
-    servinfo->ai_addrlen = res->ai_addrlen;
-    servinfo->ai_canonname = res->ai_canonname;
-    servinfo->ai_addr = res->ai_addr; // ai_addr is een structure. moeten de onderdelen daarvan ook worden gekopieerd?
-    servinfo->ai_next = NULL;
-    freeaddrinfo(res); // is nu niets dat in servinfo zit gefreed? (Is servinfo een DEEP copy van res?)
+    for (p = res; p != NULL; p = p->ai_next)
+    {
+        if (p->ai_family == AF_INET || p->ai_family == AF_INET6)
+            break;
+    }
+    memcpy(servinfo, p, sizeof(struct addrinfo));                // making copy of the result
+    memcpy(servinfo->ai_addr, p->ai_addr, servinfo->ai_addrlen); // making sure its a DEEP copy
+    freeaddrinfo(res);                                           // is nu niets dat in servinfo zit gefreed? (Is servinfo een DEEP copy van res?)
 }
 
-void add_fd_to_epoll_interest_list(int epoll_fd, int fd, std::vector<int> fds)
+void add_fd_to_epoll_interest_list(int epoll_fd, int fd)
 {
     struct epoll_event event;
     event.events = EPOLLIN;
     event.data.fd = fd;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event))
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event) == -1)
     {
+        close(server_socket);
+        close(epoll_fd);
+        close(fd);
         perror("Error adding fd to epoll interest list with epoll_ctl()");
-        for (int i = 0; i < fds.size(); i++)
-            close(fds[i]);
         exit(EXIT_FAILURE);
     }
-    if (fd != fds[0])
-        fds.push_back(fd);
+}
+
+void remove_fd_from_epoll_interest_list(int epoll_fd, int fd)
+{
+    struct epoll_event event;
+    event.events = 0;
+    event.data.fd = fd;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, &event) == -1)
+    {
+        close(server_socket);
+        close(epoll_fd);
+        close(fd);
+        perror("Error adding fd to epoll interest list with epoll_ctl()");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void siginthandler(int signum)
+{
+    if (signum == SIGINT)
+    {
+        std::cout << "SIGINT" << std::endl;
+        close(server_socket);
+        close(epoll_fd);
+        exit(EXIT_SUCCESS);
+    }
 }
 
 int start_server(struct addrinfo *servinfo)
 {
-    int server_socket;
     int request_socket = -1;
 
+    signal(SIGINT, siginthandler);
     if ((server_socket = socket(PF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("Error creating server_socket with socket()");
@@ -84,13 +102,12 @@ int start_server(struct addrinfo *servinfo)
         perror("Error staring server");
         exit(EXIT_FAILURE);
     }
-    signal(SIGINT, sigHandler);
     return (server_socket);
 }
 
 int start_epoll(int server_socket)
 {
-    int epoll_fd = epoll_create1(0);
+    epoll_fd = epoll_create1(0);
     if (epoll_fd == -1)
     {
         close(server_socket);
@@ -100,35 +117,34 @@ int start_epoll(int server_socket)
     return (epoll_fd);
 }
 
-void accept_new_connection(std::vector<int> &fds, int epoll_fd, struct addrinfo *servinfo)
-{
-    int new_client = accept(fds[0], servinfo->ai_addr, &servinfo->ai_addrlen);
-    add_fd_to_epoll_interest_list(epoll_fd, new_client, fds);
-    std::cout << "ACCEPTED NEW CONNECTION!" << std::endl;
-}
-
 void write_to_connection(int fd)
 {
     const char *server_message = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 124\n\n<html>\n <head>\n </head>\n <body>\nHey Wonderfull webserv wonderteam <3\n </body>\n</html>\n";
 
     char buffer[1024];
     int end = read(fd, buffer, 1024);
+    std::cout << "reading from socket. bytes read: " << end << std::endl;
     buffer[end] = '\0';
     std::cout << buffer << std::endl;
     write(fd, server_message, strlen(server_message));
     std::cout << "WROTE TO CONNECTION!" << std::endl;
 }
 
+void accept_new_connection(int epoll_fd, struct addrinfo *servinfo)
+{
+    int new_client = accept(server_socket, servinfo->ai_addr, &servinfo->ai_addrlen);
+    add_fd_to_epoll_interest_list(epoll_fd, new_client);
+    std::cout << "ACCEPTED NEW CONNECTION!" << std::endl;
+}
+
 int main(void)
 {
     struct addrinfo servinfo;
     getservinfo(&servinfo);
-    std::vector<int> fds;
 
-    int server_socket = start_server(&servinfo);
-    fds.push_back(server_socket);
-    int epoll_fd = start_epoll(epoll_fd);
-    add_fd_to_epoll_interest_list(epoll_fd, server_socket, fds);
+    server_socket = start_server(&servinfo);
+    epoll_fd = start_epoll(epoll_fd);
+    add_fd_to_epoll_interest_list(epoll_fd, server_socket);
     struct epoll_event events[MAX_EVENTS];
     int number_of_events;
     while ((number_of_events = epoll_wait(epoll_fd, events, MAX_EVENTS, -1)) >= 0)
@@ -136,12 +152,14 @@ int main(void)
         for (int i = 0; i < number_of_events; i++)
         {
             if (events[i].data.fd == server_socket)
-                accept_new_connection(fds, epoll_fd, &servinfo);
+                accept_new_connection(epoll_fd, &servinfo);
             else
+            {
                 write_to_connection(events[i].data.fd);
+                remove_fd_from_epoll_interest_list(epoll_fd, events[i].data.fd);
+                close(events[i].data.fd);
+            }
         }
     }
-    for (int i = 0; i < fds.size(); i++)
-        close(fds[i]);
     return (EXIT_SUCCESS);
 }
