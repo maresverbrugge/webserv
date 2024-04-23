114a8cf3fceb753e978549fdc1cb2a63cf51a1f4
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-04-23 17:32:59 +0200

    Started working on Response class

---
+++ headers/Response.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Response.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkoolhov <fkoolhov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 17:04:25 by fkoolhov          #+#    #+#             */
+/*   Updated: 2024/04/23 17:32:14 by fkoolhov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RESPONSE_HPP
+# define RESPONSE_HPP
+
+# include "webserv.hpp"
+
+class Response
+{
+	private:
+		std::vector<std::unique_ptr<Request>>	_request;
+		short									_statusCode;
+		std::string								_reasonPhrase;
+		std::map<std::string, std::string>		_responseHeaders;
+		std::string								_body;
+		std::string								_responseMessage;
+
+	public:
+		Response();
+		~Response();
+
+		void	setRequest(std::unique_ptr<Request> request);
+		void	setStatusCode(short);
+		void	setReasonPhrase(std::string);
+		void	addResponseHeader(std::string name, std::string value);
+		void	setBody(std::string);
+		void	setResponseMessage(std::string responseMessage);
+		
+		std::vector<std::unique_ptr<Request>>&	getRequest() const;
+		short									getStatusCode() const;
+		std::string								getReasonPhrase() const;
+		std::map<std::string, std::string>		getResponseHeaders() const;
+		std::string								getBody() const;
+		std::string								getResponseMessage() const;
+};
+
+#endif
---
+++ src/Response.cpp
@@ -0,0 +1,23 @@
+// reponse is either requested file or error message
+
+// header
+//	status line
+//		http version
+//		status code
+//		reason phrase
+//	optional response headers
+//		name:value
+//		name:value
+
+// blank line
+
+// body (optional)
+//	html
+
+
+
+// if GET
+// search path in server locations
+// read file
+// create response
+// return response
