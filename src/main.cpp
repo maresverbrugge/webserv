cbb55c579305d7acad7c4d3e1f9722eb2fd9d7c3
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-05-08 18:55:55 +0200

    Working on post request parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   main.cpp                                           :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: fkoolhov <fkoolhov@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2023/12/18 15:01:35 by mverbrug      #+#    #+#                 */
-/*   Updated: 2024/05/07 16:35:32 by fhuisman      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkoolhov <fkoolhov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 15:01:35 by mverbrug          #+#    #+#             */
+/*   Updated: 2024/05/08 18:54:50 by fkoolhov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,26 @@
 
 int main(int argc, char** argv)
 {
-	std::string http_request = "GET /cgi.html?mares=lief&felicia=mooi&flen=cool#section-3.6 HTTP/1.1\r\nheaderName: headerValue\r\n\t..continuation\r\n\t...CONTI\%4EUATIO\%4e\r\nContent-Length: 68\r\nheaderName: otherValue\r\nheaderName3:\r\nHost: localhost:8080\r\n\r\nThis is the body of the request\r\nThis is the body of the request\r\n\r\n";
+	// std::string http_request = "GET /cgi.html?mares=lief&felicia=mooi&flen=cool#section-3.6 HTTP/1.1\r\nheaderName: headerValue\r\n\t..continuation\r\n\t...CONTI\%4EUATIO\%4e\r\nContent-Length: 68\r\nheaderName: otherValue\r\nheaderName3:\r\nHost: localhost:8080\r\n\r\nThis is the body of the request\r\nThis is the body of the request\r\n\r\n";
+	std::string http_request = "POST /path HTTP/1.1\r\nHost: example.com\r\nTransfer-Encoding: chunked\r\n\r\n"
+									  "6\r\n"
+									  "Hello \r\n"
+									  "9\r\n"
+									  "World! \r\n\r\n"
+									  "7\r\n"
+									  "Chunks!\r\n"
+									  "0\r\n"
+									  "\r\n";
+	// std::string http_request = "POST /path HTTP/1.1\r\nHost: example.com\r\nContent-Length: 16\r\n\r\n"
+	// 						   "Hello World!\nyay\r\n";
 
 	if (argc != 2)
 	{
 		// handle error
 		return (EXIT_FAILURE);
 	}
-	
 	std::unique_ptr<ServerPool> serverpool = configure_serverpool(argv[1]);
-	std::cout << *serverpool << std::endl; // for debugging purposes
+	// std::cout << *serverpool << std::endl; // for debugging purposes
 	try 
 	{
 		std::unique_ptr<Request> request = std::make_unique<Request>(http_request);
--- src/request/parsePostRequest.cpp
+++ src/request/parsePostRequest.cpp
@@ -1,60 +1,117 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   parsePostRequest.cpp                               :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: fhuisman <fhuisman@student.codam.nl>         +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2024/05/01 11:01:45 by fhuisman      #+#    #+#                 */
-/*   Updated: 2024/05/02 11:15:18 by fhuisman      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   parsePostRequest.cpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkoolhov <fkoolhov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 11:01:45 by fhuisman          #+#    #+#             */
+/*   Updated: 2024/05/08 18:48:11 by fkoolhov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Request.hpp"
 
-static void get_body(std::stringstream &ss)
+static void parse_chunked_body(std::stringstream& stringstream, std::ofstream& outfile)
 {
-    std::string line;
-    std::ofstream body;
-    std::string filename = "./root/upload/body"; // make function to get the right name?
+	std::cout << "Parsing chunked body" << std::endl; // for debugging purposes
+	std::string line;
+	
+	try
+	{
+		while (std::getline(stringstream, line))
+		{
+			if (line == "0")
+			{
+				break;
+			}
+			std::cout << "line: " << line << std::endl; // for debugging purposes
+			int chunk_size = std::stoi(line, nullptr, 0x16);
+			if (chunk_size == 0)
+			{
+				break;
+			}
+			std::string chunk;
+			chunk.resize(chunk_size);
+			stringstream.read(&chunk[0], chunk_size);
+			outfile.write(chunk.c_str(), chunk_size);
+			std::getline(stringstream, line); // read the empty line after each chunk
+		}
+		outfile.close();
+	}
+	catch(const std::exception& exception)
+	{
+		outfile.close();
+		throw (400); // bad request (for example if stoi fails)
+	}
+}
 
-    std::getline(ss, line);
-    if (line != "" && line != "\r")
-        body.open(filename);
-    //check if file is open?
-    while (line != "" && line != "\r")
-    {
-        trim_cr(line);
-        body << line << std::endl;
-        std::getline(ss, line);
-    }
-    body.close();
+static void parse_identity_body(std::stringstream& stringstream, std::ofstream& outfile, int content_length)
+{
+	std::cout << "Parsing identity body" << std::endl; // for debugging purposes
+	std::string line;
+	
+	try
+	{
+		std::string body;
+		body.resize(content_length);
+		stringstream.read(&body[0], content_length);
+		outfile.write(body.c_str(), content_length);
+    	outfile.close();
+	}
+	catch(const std::exception& exception)
+	{
+		outfile.close();
+		throw (400); // Bad Request
+	}
 }
 
 static void get_content_length(Request *request)
 {
-    std::string content_lenth;
     auto headers = request->getHeaders();
-    auto it = headers.find("content-encoding");
-    if (it == headers.end() || (*it).second == "identity")
+    std::string transfer_encoding = headers["transfer-encoding"];
+
+    if (transfer_encoding == "" || transfer_encoding == "identity")
     {
-        it = headers.find("content-length");
-        if (it == headers.end())
+        std::string content_length = headers["content-length"];
+        if (content_length == "")
         {
-            if (request->getMethod() == POST)
-                throw (411);
+            throw (411); // Length Required
         }
         else
         {
-            content_lenth = (*it).second;
-            request->setContentLength(std::atoi(content_lenth.c_str()));
+            request->setContentLength(std::atoi(content_length.c_str()));
         }
     }
     else request->setContentLength(-1);
 }
 
-void Request::parsePostRequest(std::stringstream& ss)
+void Request::parsePostRequest(std::stringstream& stringstream)
 {
-    get_body(ss);
+    std::cout << "Parsing POST request" << std::endl;
     get_content_length(this);
+
+    std::string transfer_encoding = this->_headers["transfer-encoding"];
+    if (transfer_encoding != "" && transfer_encoding != "chunked" && transfer_encoding != "identity") 
+	{
+    	throw (501); // Not Implemented
+	}
+
+	std::string filename = "./root/upload/body"; // make function to get the right name?
+	std::ofstream outfile;
+
+	outfile.open(filename);
+	if (!outfile.is_open())
+	{
+		throw (500); // Internal server error
+	}
+
+	if (transfer_encoding == "chunked")
+	{
+		parse_chunked_body(stringstream, outfile);
+	}
+	else
+	{
+		parse_identity_body(stringstream, outfile, this->_contentLength);
+	}
 }
