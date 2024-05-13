563a82d43389e84397748cfd02acc48bb0a4184a
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-05-13 15:10:58 +0200

    Replaced old header with new header

--- src/request/parseURI.cpp
+++ src/request/parseURI.cpp
@@ -1,3 +1,19 @@
+/* ************************************************************************* */
+/*      ##       ##      ## ##       ##      ## ##       ##      ##          */
+/*       ##     ####    ##   ##     ####    ##   ##     ####    ##           */
+/*        ##  ##   ##  ##     ##  ##   ##  ##     ##  ##   ##  ##            */
+/*         ####     ####       ####     ####       ####     ####             */
+/*          ##       ##         ##       ##         ##       ##              */
+/*                                                                           */
+/*           WONDERFUL            WEBSERV           WONDERTEAM               */
+/*                                                                           */
+/*      FELICIA KOOLHOVEN      FLEN HUISMAN       MARES VERBRUGGE            */
+/*          fkoolhov             fhuisman             mverbrug               */
+/*                                                                           */
+/*          Codam Coding College        part of 42 network                   */
+/*                            April - May 2024                               */
+/* ************************************************************************* */
+
 #include <iostream>
 #include <string>
 #include "Request.hpp"
