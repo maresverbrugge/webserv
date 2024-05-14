7281d534d8c6dfb7d54124c878920f19d8057cc9
fkoolhov <fkoolhov@student.codam.nl>
2024-05-14 18:14:57 +0200

    Request class handles multipart/form-data, not verified

--- src/request/parsePostRequest.cpp
+++ src/request/parsePostRequest.cpp
@@ -16,7 +16,71 @@
 
 #include "Request.hpp"
 
-static void parse_chunked_body(Request *request, std::stringstream& stringstream)
+static void add_file_headers_to_request(Request* request, std::string header)
+{
+	std::map<std::string, std::string> headers;
+	std::stringstream header_stream(header);
+	std::string line;
+	std::getline(header_stream, line); // skip the empty line after the boundary?
+	while (std::getline(header_stream, line))
+	{
+		size_t colon = line.find(":");
+		if (colon == std::string::npos)
+			throw (BAD_REQUEST);
+		std::string key = line.substr(0, colon);
+		std::string value = line.substr(colon + 2);
+		std::transform(key.begin(), key.end(), key.begin(), ::tolower);
+		request->setHeader(key, value);
+	}
+}
+
+static std::string get_first_part(std::string body, std::string delimiter)
+{
+	size_t pos = body.find(delimiter);
+	if (pos == std::string::npos)
+		throw (BAD_REQUEST);
+	pos += delimiter.size();
+	size_t end = body.find(delimiter, pos);
+	if (end == std::string::npos)
+		throw (BAD_REQUEST);
+	std::string first_part = body.substr(pos, end - pos);
+	return first_part;
+}
+
+static std::string get_delimiter(Request* request)
+{
+	std::string boundary = "";
+	std::map<std::string, std::string> headers = request->getHeaders();
+	auto it = headers.find("content-type");
+	if (it != headers.end())
+	{
+		std::string content_type = it->second;
+		size_t pos = content_type.find("boundary=");
+		if (pos != std::string::npos)
+			boundary = content_type.substr(pos + 9);
+	}
+	if (boundary == "")
+		throw (BAD_REQUEST);
+	std::string delimiter = "--" + boundary;
+	return delimiter;
+}
+
+static void parse_multipart_form_data(Request* request) // check if \r\n is handled correctly
+{
+	std::string delimiter = get_delimiter(request);
+	std::string body = request->getBody();
+	std::string first_part = get_first_part(body, delimiter);
+	size_t header_end = first_part.find("\r\n\r\n");
+	if (header_end == std::string::npos)
+		throw (BAD_REQUEST);
+	std::string header = first_part.substr(0, header_end);
+	std::string content = first_part.substr(header_end + 4); 
+	add_file_headers_to_request(request, header);
+	request->setBody(content);
+	request->setContentLength(content.size()); // not accidentally including \r\n?
+}
+
+static void parse_chunked_body(Request* request, std::stringstream& stringstream)
 {
 	try
 	{
@@ -47,7 +111,7 @@ static void parse_chunked_body(Request *request, std::stringstream& stringstream
 	}
 }
 
-static void parse_identity_body(Request *request, std::stringstream& stringstream)
+static void parse_identity_body(Request* request, std::stringstream& stringstream)
 {
 	try
 	{
@@ -62,7 +126,7 @@ static void parse_identity_body(Request *request, std::stringstream& stringstrea
 	}
 }
 
-static void get_content_length(Request *request, std::string transfer_encoding)
+static void get_content_length(Request* request, std::string transfer_encoding)
 {
     std::map<std::string, std::string> headers = request->getHeaders();
 
@@ -88,18 +152,27 @@ static std::string verify_transfer_encoding(std::map<std::string, std::string> h
 	if (it != headers.end())
 		transfer_encoding = it->second;
     if (transfer_encoding != "" && transfer_encoding != "chunked" && transfer_encoding != "identity") 
-		throw (NOT_IMPLEMENTED); // Not Implemented
+		throw (NOT_IMPLEMENTED);
 	return transfer_encoding;
 }
 
 void Request::parsePostRequest(std::stringstream& stringstream)
 {
-    std::cout << "Parsing POST request" << std::endl;
 	std::string transfer_encoding = verify_transfer_encoding(this->_headers);
     get_content_length(this, transfer_encoding);
 
+	// handle \0 in images?
 	if (transfer_encoding == "chunked")
 		parse_chunked_body(this, stringstream);
 	else
 		parse_identity_body(this, stringstream);
+
+	std::string content_type = "";
+	auto it = this->_headers.find("content-type");
+	if (it != this->_headers.end())
+		content_type = it->second;
+	if (content_type == "")
+		throw (BAD_REQUEST);
+	if (content_type.find("multipart/form-data") != std::string::npos)
+		parse_multipart_form_data(this);
 }
