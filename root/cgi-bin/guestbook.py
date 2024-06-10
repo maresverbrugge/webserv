897893d6dfc9eaa98f2cc9c86ae65da8b786dab0
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-06-10 13:42:32 +0200

    Better error handling in Python scripts

--- root/cgi-bin/guestbook.py
+++ root/cgi-bin/guestbook.py
@@ -1,47 +1,58 @@
 #!/usr/bin/env python3
 import cgi
 import os
-# Path to store messages
-MESSAGE_FILE = "./root/guestbook/guestbook.txt"
-def save_message(name, message):
-    with open(MESSAGE_FILE, 'a') as file:
-        file.write(f"{name}: {message}\n\n")
-def read_messages():
-    if not os.path.exists(MESSAGE_FILE):
-        return ""
-    with open(MESSAGE_FILE, 'r') as file:
-        return file.read().replace("\n", "<br>")
-def main():
-    print("HTTP/1.1 200 OK\r\nContent-type: text/html\r\n")
-    name = os.environ.get('name', '')
-    message = os.environ.get('message', '')
-    message = message.replace('+', ' ')
-    if name and message:
-        save_message(name, message)
-    messages = read_messages()
-    print(f"""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Guestbook</title>
-        <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
-	    <link rel="stylesheet" href="/styles/styles.css">
-    </head>
-    <body>
-        <h1>Guestbook</h1>
-        go back to the <a href="/pages/index.html">homepage</a><br><br>
-        <form action="/cgi-bin/guestbook.py" method="post">
-            <label for="name">Name:</label><br>
-            <input type="text" id="name" name="name"><br>
-            <label for="message">Message:</label><br>
-            <textarea id="message" name="message"></textarea><br>
-            <input type="submit" value="Submit">
-        </form>
-        <h2>Messages</h2>
-        <div id="messages">{messages}</div>
-    </body>
-    </html>
-    """)
-if __name__ == "__main__":
-    main()
+import sys
+
+# Ignore SIGPIPE
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+
+try:
+    # Path to store messages
+    MESSAGE_FILE = "./root/guestbook/guestbook.txt"
+    def save_message(name, message):
+        with open(MESSAGE_FILE, 'a') as file:
+            file.write(f"{name}: {message}\n\n")
+    def read_messages():
+        if not os.path.exists(MESSAGE_FILE):
+            return ""
+        with open(MESSAGE_FILE, 'r') as file:
+            return file.read().replace("\n", "<br>")
+    def main():
+        print("HTTP/1.1 200 OK\r\nContent-type: text/html\r\n")
+        name = os.environ.get('name', '')
+        message = os.environ.get('message', '')
+        message = message.replace('+', ' ')
+        if name and message:
+            save_message(name, message)
+        messages = read_messages()
+        print(f"""
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Guestbook</title>
+            <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+            <link rel="stylesheet" href="/styles/styles.css">
+        </head>
+        <body>
+            <h1>Guestbook</h1>
+            go back to the <a href="/pages/index.html">homepage</a><br><br>
+            <form action="/cgi-bin/guestbook.py" method="post">
+                <label for="name">Name:</label><br>
+                <input type="text" id="name" name="name"><br>
+                <label for="message">Message:</label><br>
+                <textarea id="message" name="message"></textarea><br>
+                <input type="submit" value="Submit">
+            </form>
+            <h2>Messages</h2>
+            <div id="messages">{messages}</div>
+        </body>
+        </html>
+        """)
+        while (True):
+            print("This is a bug")
+    if __name__ == "__main__":
+        main()
+except Exception:
+    sys.stderr.close()
+    exit()
--- root/cgi-bin/upload_and_delete.py
+++ root/cgi-bin/upload_and_delete.py
@@ -11,34 +11,34 @@ def generate_http_response():
     
     # HTML template
     html_template = '''<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload and Delete</title>
-    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
-    <link rel="stylesheet" href="/styles/styles.css">
-    <script src="/scripts/upload.js"></script>
-    <script src="/scripts/delete.js"></script>
-</head>
-<body>
-    <div class="container">
-        <h1>Upload and Delete</h1>
-        <p>
-            Would you like to upload something or go back to the <a href="/pages/index.html">homepage</a>?
-        </p>
-        <form id="uploadForm">
-            <input type="file" name="file" id="fileInput">
-            <input type="button" value="Upload" onclick="uploadFile()">
-        </form>
-        <h2>List of Files</h2>
-        <ul>
-            {file_list}
-        </ul>
-    </div>
-</body>
-</html>
-'''
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Upload and Delete</title>
+        <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+        <link rel="stylesheet" href="/styles/styles.css">
+        <script src="/scripts/upload.js"></script>
+        <script src="/scripts/delete.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Upload and Delete</h1>
+            <p>
+                Would you like to upload something or go back to the <a href="/pages/index.html">homepage</a>?
+            </p>
+            <form id="uploadForm">
+                <input type="file" name="file" id="fileInput">
+                <input type="button" value="Upload" onclick="uploadFile()">
+            </form>
+            <h2>List of Files</h2>
+            <ul>
+                {file_list}
+            </ul>
+        </div>
+    </body>
+    </html>
+    '''
 
     # Generate the list of files with delete buttons
     file_list_html = ''
@@ -51,10 +51,9 @@ def generate_http_response():
                 <button onclick="deleteFile('/uploads/{filename}')">Delete</button>
             </li>
             '''
-
     # Fill the template with the file list
     html_content = html_template.format(file_list=file_list_html)
-    
+
     # Generate HTTP response
     response = "HTTP/1.1 200 OK\r\n"
     response += f"Content-Length: {len(html_content)}\r\n"
@@ -62,13 +61,13 @@ def generate_http_response():
     response += "Connection: close\r\n"
     response += "\r\n"
     response += html_content
-    
+
     # Write the generated response to standard output
-    try:
-        sys.stdout.write(response)
-    except BrokenPipeError:
-        sys.stderr.close()
-        exit()
+    sys.stdout.write(response)
 
 # Call the function to generate the HTTP response
-generate_http_response()
+try:
+    generate_http_response()
+except Exception:
+    sys.stderr.close()
+    exit()
