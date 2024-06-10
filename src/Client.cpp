feecea257ef61b492fea34f66f36b3c6cf37dca1
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-06-10 11:43:37 +0200

    Removed debug output in requestIsComplete()

--- src/Client.cpp
+++ src/Client.cpp
@@ -85,17 +85,13 @@ bool Client::headersComplete()
 
 bool Client::requestIsComplete()
 {
-	if(_request->getTransferEncoding() == CHUNKED) // check if this works? how?
+	if(_request->getTransferEncoding() == CHUNKED)
 	{
 		if (_fullBuffer.find("\r\n0\r\n\r\n") != std::string::npos)
-		{
-			std::cout << RED BOLD "Request is complete\n" RESET;	
 			return true;
-		}
-		std::cout << RED BOLD "Request is not complete\n" RESET;
 		return false;
 	}
-	if (_fullBuffer.size() - (_fullBuffer.find("\r\n\r\n") + strlen("\r\n\r\n")) >= _request->getContentLength())
+	else if (_fullBuffer.size() - (_fullBuffer.find("\r\n\r\n") + strlen("\r\n\r\n")) >= _request->getContentLength())
 		return true;
 	else
 		return false;
