9972934324e0f9893c3529cbf7082c9a56cc8903
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-06-10 12:37:04 +0200

    Created sockets folder, put exception definitions in cpp files

--- src/generalUtils.cpp
+++ src/generalUtils.cpp
@@ -65,3 +65,26 @@ void set_fd_to_non_blocking_and_cloexec(int fd)
 	set_to_non_blocking(fd);
 	set_to_cloexec(fd);
 }
+
+// A StatusCodeException is thrown when a status code is not 200 OK.
+StatusCodeException::StatusCodeException(const std::string& message, const e_status& status_code)
+	: _message(message), _status_code(status_code) {}
+
+const char* StatusCodeException::what() const noexcept
+{
+	return _message.c_str();
+}
+
+const e_status& StatusCodeException::status() const noexcept
+{
+	return _status_code;
+}
+
+// A FatalException is thrown when a fatal error occurs.
+FatalException::FatalException(const std::string& message) 
+	: _message(message) {}
+
+const char* FatalException::what() const noexcept 
+{
+	return _message.c_str();
+}
