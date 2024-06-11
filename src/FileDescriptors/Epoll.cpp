58642846cdc7cc88b1887c876660f7f1dea93b5b
Felicia Koolhoven <fmkoolhoven@gmail.com>
2024-06-11 09:41:59 +0200

    Epoll loop first checks out events

--- src/FileDescriptors/Epoll.cpp
+++ src/FileDescriptors/Epoll.cpp
@@ -100,14 +100,6 @@ int Epoll::handleInEvents(AFileDescriptor* ptr)
 			return (ERROR);
 		}
 	}
-	else if (client && client->getReadyForFlag() == WRITE) // TALK TO MARES AND FLEN, do this or read all requests first? some other solution???
-	{
-		if (!client->getResponse().empty())
-		{
-			client->writeToClient();
-			client->getServer().removeClientConnection(client);
-		}
-	}
 	else if (signal)
 		signal->readSignal();
 	else if (cgi)
@@ -117,10 +109,12 @@ int Epoll::handleInEvents(AFileDescriptor* ptr)
 		else
 			cgi->getClient().deleteCGI();
 	}
+	else
+		client->getServer().removeClientConnection(client);
 	return (SUCCESS);
 }
 
-void Epoll::handleOutEvents(AFileDescriptor* ptr)
+int Epoll::handleOutEvents(AFileDescriptor* ptr)
 {
 	Client *client = dynamic_cast<Client *>(ptr);
 	CGI *cgi = dynamic_cast<CGI *>(ptr);
@@ -129,9 +123,14 @@ void Epoll::handleOutEvents(AFileDescriptor* ptr)
 	{
 		client->writeToClient();
 		client->getServer().removeClientConnection(client);
+		return SUCCESS;
 	}
 	else if (cgi && _isChildProcess)
+	{
 		runScript(cgi);
+		return SUCCESS;
+	}
+	return ERROR;
 }
 
 void Epoll::EpollLoop()
@@ -146,14 +145,15 @@ void Epoll::EpollLoop()
 			throw FatalException("epoll_wait() failed");
 		for (int i = 0; i < epoll_return; i++)
 		{
+			int out_event_success = ERROR;
 			ready_listDataPtr = static_cast<AFileDescriptor *>(event_list[i].data.ptr);
-			if (event_list[i].events & EPOLLIN && !_isChildProcess)
+			if (event_list[i].events & EPOLLOUT)
+				out_event_success = handleOutEvents(ready_listDataPtr);
+			if (out_event_success == ERROR && event_list[i].events & EPOLLIN && !_isChildProcess)
 			{
 				if (handleInEvents(ready_listDataPtr) != SUCCESS)
 					break; // testen met meerdere clients
 			}
-			else if (event_list[i].events & EPOLLOUT)
-				handleOutEvents(ready_listDataPtr);
 		}
 	}
 }
